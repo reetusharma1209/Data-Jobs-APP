4a8235e36f36c12743bfe07c1380a73b98a00097
reetusharma1209 <reetusharma1209@gmail.com>
2024-08-23 00:05:58 +0200

    a

--- app.py
+++ app.py
@@ -50,6 +50,7 @@ def process_skills(skills_string):
     skills_list = [skill for skill in skills_list if skill not in ['not', 'specified']]
     return skills_list
 
+@st.cache_data
 def recent_job_market(df_cleaned, eda_option):
     st.title("🌟 Recent Data Job Market")
 
